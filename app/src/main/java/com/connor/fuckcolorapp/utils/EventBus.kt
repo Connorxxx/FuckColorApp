fd2668ebe4a01a1e2f716efde0f7c24f22655293
connor <wyh2542367414d@gmail.com>
2023-06-03 01:34:29 +0800

    去掉粘性

--- app/src/main/java/com/connor/fuckcolorapp/utils/EventBus.kt
+++ app/src/main/java/com/connor/fuckcolorapp/utils/EventBus.kt
@@ -1,20 +1,11 @@
 package com.connor.fuckcolorapp.utils
 
 import com.connor.fuckcolorapp.states.Event
-import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableSharedFlow
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 val eventBus = MutableSharedFlow<Event>()
-val stickEvent = MutableSharedFlow<Event>(replay = 1)
-fun CoroutineScope.subscribe(block: suspend (Event) -> Unit) {
-    launch { eventBus.collect(block) }
-    launch { stickEvent.collect(block) }
-}
+suspend fun subscribe(block: suspend (Event) -> Unit) { eventBus.collect(block) }
 
-suspend fun post(event: Event, isStick: Boolean = false) = withContext(Dispatchers.IO) {
-    if (!isStick) eventBus.emit(event)
-    else stickEvent.emit(event)
-}
+suspend fun post(event: Event) = withContext(Dispatchers.IO) { eventBus.emit(event) }
